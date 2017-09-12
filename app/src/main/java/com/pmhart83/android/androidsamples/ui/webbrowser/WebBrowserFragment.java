4f26e6909d62c73a4a218619db5477043a2888f3
Paul Hart <pmhart@ymail.com>
2017-09-11 21:58:55 -0500

    added back and fwd buttons to web fragment

--- app/src/main/java/com/pmhart83/android/androidsamples/ui/webbrowser/WebBrowserFragment.java
+++ app/src/main/java/com/pmhart83/android/androidsamples/ui/webbrowser/WebBrowserFragment.java
@@ -19,6 +19,8 @@ public class WebBrowserFragment extends Fragment implements View.OnClickListener
 
     private WebView _webView;
     private EditText _urlEditText;
+    private Button _backButton;
+    private Button _fwdButton;
 
     public WebBrowserFragment() {
     }
@@ -33,6 +35,23 @@ public class WebBrowserFragment extends Fragment implements View.OnClickListener
     {
         _homeUrl = getString(R.string.url_webbrowser_home);
 
+        Button buttonHome = view.findViewById(R.id.button_home);
+        buttonHome.setOnClickListener(this);
+
+        Button buttonGo = view.findViewById(R.id.button_go);
+        buttonGo.setOnClickListener(this);
+
+        Button buttonRefresh = view.findViewById(R.id.button_refresh);
+        buttonRefresh.setOnClickListener(this);
+
+        _backButton = view.findViewById(R.id.button_back);
+        _backButton.setOnClickListener(this);
+        _backButton.setEnabled(false);
+
+        _fwdButton = view.findViewById(R.id.button_forward);
+        _fwdButton.setOnClickListener(this);
+        _fwdButton.setEnabled(false);
+
         _webView = (WebView) view.findViewById(R.id.webview);
         _webView.setWebViewClient(new WebViewClient() {
             @Override
@@ -44,15 +63,6 @@ public class WebBrowserFragment extends Fragment implements View.OnClickListener
 
         _urlEditText = view.findViewById(R.id.entry_url);
         _urlEditText.setText(_homeUrl);
-
-        Button buttonHome = view.findViewById(R.id.button_home);
-        buttonHome.setOnClickListener(this);
-
-        Button buttonGo = view.findViewById(R.id.button_go);
-        buttonGo.setOnClickListener(this);
-
-        Button buttonRefresh = view.findViewById(R.id.button_refresh);
-        buttonRefresh.setOnClickListener(this);
     }
 
     private void handleOnPageFinished(WebView view)
@@ -61,19 +71,52 @@ public class WebBrowserFragment extends Fragment implements View.OnClickListener
         {
             _urlEditText.setText(view.getUrl());
         }
+
+        if(_backButton != null)
+        {
+            _backButton.setEnabled(view.canGoBack());
+        }
+
+        if(_fwdButton != null)
+        {
+            _fwdButton.setEnabled(view.canGoForward());
+        }
     }
 
     public void onClick(final View v) {
         switch (v.getId()) {
             case R.id.button_home:
+
                 goUrl(_homeUrl);
                 break;
+
             case R.id.button_go:
+
                 goCurrentUrl();
                 break;
+
             case R.id.button_refresh:
+
                 reload();
                 break;
+
+            case R.id.button_back:
+
+                if(_webView != null && _webView.canGoBack())
+                {
+                    _webView.goBack();
+                }
+                break;
+
+            case R.id.button_forward:
+
+                if(_webView != null && _webView.canGoForward())
+                {
+                    _webView.goForward();
+                }
+
+                break;
+
             default:
                 break;
         }
